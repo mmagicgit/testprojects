753234d7ec259df69cd11e4e6a138e501171b4f8
Marcel Schneider <marcel.schneider@idos.de>
2013-10-10 16:09:47 +0200

    Test erweitert

--- arquillian-test/src/main/java/example/GreeterEjb.java
+++ arquillian-test/src/main/java/example/GreeterEjb.java
@@ -1,8 +1,16 @@
 package example;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 
 @Stateless
 public class GreeterEjb {
+
+	@Inject
+	private Greeter greeter;
+
+	public Greeter getGreeter() {
+		return greeter;
+	}
 
 }
--- arquillian-test/src/test/java/example/GreeterTest.java
+++ arquillian-test/src/test/java/example/GreeterTest.java
@@ -42,5 +42,6 @@ public class GreeterTest {
 	@Test
 	public void ejbInjectedGreeter() throws Exception {
 		assertNotNull(greeterEjb);
+		assertNotNull(greeterEjb.getGreeter());
 	}
 }
