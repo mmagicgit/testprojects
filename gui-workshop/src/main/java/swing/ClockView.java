9b0e530711af90d0c5150698f6e53ae6b7a0d252
Marcel Schneider <marcel.schneider@idos.de>
2014-06-05 11:14:04 +0200

    Button zum Zeitanhalten eingebaut

--- gui-workshop/src/main/java/swing/ClockView.java
+++ gui-workshop/src/main/java/swing/ClockView.java
@@ -1,8 +1,10 @@
 package swing;
 
+import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.util.List;
 
+import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
 
@@ -10,18 +12,22 @@ import swing.city.CityClockView;
 
 public class ClockView {
 
-	private final JPanel panel;
+	private final JPanel mainPanel = new JPanel(new BorderLayout());
+	private final JButton button = new JButton("doIt");
+	private final JPanel clockPanel;
 
 	public ClockView(List<CityClockView> subViews) {
-		panel = new JPanel(new GridLayout(subViews.size(), 2, 10, 10));
+		clockPanel = new JPanel(new GridLayout(subViews.size(), 2, 10, 10));
 		for (CityClockView singleClockView : subViews) {
-			panel.add(singleClockView.getTitleComponent());
-			panel.add(singleClockView.getTimeComponent());
+			clockPanel.add(singleClockView.getTitleComponent());
+			clockPanel.add(singleClockView.getTimeComponent());
 		}
+		mainPanel.add(clockPanel, BorderLayout.CENTER);
+		mainPanel.add(button, BorderLayout.SOUTH);
 	}
 
 	public JComponent getComponent() {
-		return panel;
+		return mainPanel;
 	}
 
 }
