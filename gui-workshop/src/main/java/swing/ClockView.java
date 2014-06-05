388484b753205bcb189b5b8a0d43da3222f8c335
Marcel Schneider <marcel.schneider@idos.de>
2014-06-05 11:04:01 +0200

    View flexibler gemacht

--- gui-workshop/src/main/java/swing/ClockView.java
+++ gui-workshop/src/main/java/swing/ClockView.java
@@ -10,9 +10,10 @@ import swing.city.CityClockView;
 
 public class ClockView {
 
-	private final JPanel panel = new JPanel(new GridLayout(5, 2, 10, 10));
+	private final JPanel panel;
 
 	public ClockView(List<CityClockView> subViews) {
+		panel = new JPanel(new GridLayout(subViews.size(), 2, 10, 10));
 		for (CityClockView singleClockView : subViews) {
 			panel.add(singleClockView.getTitleComponent());
 			panel.add(singleClockView.getTimeComponent());
