6f58e6bb0c0efb841b987c46f4b290681aac0ad1
Marcel Schneider <marcel.schneider@idos.de>
2015-06-15 13:13:29 +0200

    Konstanten eingeführt

--- vier-gewinnt-craftyjs/game.js
+++ vier-gewinnt-craftyjs/game.js
@@ -2,29 +2,31 @@ Crafty.init(500, 350, document.getElementById('game'));
 Crafty.e('Floor, 2D, Canvas, Color').attr({x: 0, y: 350, w: 350, h: 1}).color('#F00');
 
 var player = "Schwarz";
+var maxColumn = 6;
+var maxLine = 5;
 var matrix = [[]];
 
-for (var column = 0; column < 7; column++) {
+for (var column = 0; column <= maxColumn; column++) {
     matrix[column] = [];
-    for (var line = 0; line < 6; line++) {
+    for (var line = 0; line <= maxLine; line++) {
         matrix[column][line] = "Empty";
     }
 }
 
 Crafty.bind("KeyDown", function (e) {
     var column = e.key - 49;
-    if (column < 0 || column > 6) {
+    if (column < 0 || column > maxColumn) {
         return;
     }
     var line = nextLineIndex(matrix[column]);
-    if (line > 5) {
+    if (line > maxLine) {
         return;
     }
 
     var squareColor = player == "Schwarz" ? '#F00' : 'green';
-    var gravityTo = line == 0 ? 'Floor' : "x" + column + "" + (line - 1);
+    var gravityTo = line == 0 ? 'Floor' : createItemName(column, line - 1);
 
-    Crafty.e("x" + column + "" + line + ', 2D, Canvas, Color, Gravity').attr({
+    Crafty.e(createItemName(column, line) + ', 2D, Canvas, Color, Gravity').attr({
         x: column * 50,
         y: 0,
         w: 50,
@@ -40,13 +42,17 @@ Crafty.bind("KeyDown", function (e) {
     player = (player == "Schwarz" ? "Gruen" : "Schwarz");
 });
 
+function createItemName(column, line) {
+    return "x" + column + "" + (line);
+}
+
 function nextLineIndex(columnArray) {
-    for (var index = 0; index < 6; index++) {
+    for (var index = 0; index < maxColumn; index++) {
         if (columnArray[index] == "Empty") {
             return index;
         }
     }
-    return 6;
+    return maxColumn;
 }
 
 function checkWinner(column, line) {
@@ -67,7 +73,7 @@ function checkVertical(column, line) {
 
 function checkHorizontal(line) {
     var counter = 0;
-    for (var index = 0; index < 6; index++) {
+    for (var index = 0; index < maxColumn; index++) {
         if (matrix[index][line] == player) {
             counter++;
             if (counter == 4) {
@@ -88,7 +94,7 @@ function checkAscendingDiagonal(column, line) {
         startLine = startLine - 1;
     }
     var counter = 0;
-    for (; startColumn < 7 && startLine < 6; startColumn++, startLine++) {
+    for (; startColumn <= maxColumn && startLine <= maxLine; startColumn++, startLine++) {
         if (matrix[startColumn][startLine] == player) {
             counter++;
             if (counter == 4) {
@@ -104,12 +110,12 @@ function checkAscendingDiagonal(column, line) {
 function checkDescendingDiagonal(column, line) {
     var startColumn = column;
     var startLine = line;
-    while (startColumn < 6 && startLine > 0) {
+    while (startColumn < maxColumn && startLine > 0) {
         startColumn = startColumn + 1;
         startLine = startLine - 1;
     }
     var counter = 0;
-    for (; startColumn >= 0 && startLine < 6; startColumn--, startLine++) {
+    for (; startColumn >= 0 && startLine <= maxLine; startColumn--, startLine++) {
         if (matrix[startColumn][startLine] == player) {
             counter++;
             if (counter == 4) {
