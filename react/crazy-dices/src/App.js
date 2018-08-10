dbfa061b5c3271dac5998367a261d4f5c03781fe
Marcel Schneider <marcel.sch@web.de>
2018-08-10 18:36:36 +0200

    Simulation of single and multiple dice throws

--- react/crazy-dices/src/App.js
+++ react/crazy-dices/src/App.js
@@ -1,18 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    singleResult: 0,
+    multipleResult: "0"
+  };
+
+  calculateSingle = () => {
+    this.setState({
+      singleResult: this.refs.selected_number_single.value * this.randomNumber(this.refs.selected_type_single.value) + parseInt(this.refs.offset_single.value)
+    });
+  };
+
+  calculateMultiple = () => {
+    var result = "";
+    for(var i=0; i < this.refs.selected_number_multiple.value; i++) {
+      result += this.randomNumber(this.refs.selected_type_multiple.value) + " ";
+    }
+    this.setState({
+      multipleResult: result
+    });
+  };
+
+  randomNumber = (max) => {
+    return Math.floor(Math.random() * max) + 1;
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Crazy Dices</h1>
+
+        <h2>Single Throw</h2>
+        <select size="1" ref="selected_number_single">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+        </select>
+        x
+        W
+        <select size="1" ref="selected_type_single">
+            <option>6</option>
+            <option>20</option>
+        </select>
+        +
+        <input size="1" defaultValue="0" type="text" ref="offset_single"/>
+        =
+        {this.state.singleResult}
+        <button onClick={this.calculateSingle}>Go</button>
+
+        <h2>Multiple Throw</h2>
+        <select size="1" ref="selected_number_multiple">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+        </select>
+        x
+        W
+        <select size="1" ref="selected_type_multiple">
+            <option>6</option>
+            <option>20</option>
+        </select>
+        =
+        {this.state.multipleResult}
+        <button onClick={this.calculateMultiple}>Go</button>
       </div>
     );
   }
